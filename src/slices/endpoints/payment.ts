3a0ceb5a067bf3cbba2332596b22327ac20b621e
griffinodow <griffin@griffindow.com>
2022-01-05 14:54:24 -0800

    Add comment to payment endpoint

--- src/slices/endpoints/payment.ts
+++ src/slices/endpoints/payment.ts
@@ -1,19 +1,24 @@
 import { v4 as uuidv4 } from 'uuid'
 import { apiSlice } from '../api'
 
+/**
+ * Payment endpoint in the api slice.
+ */
 const paymentApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    createPayment: builder.mutation<void, { sourceId: string, orderId: string }>({
-      query: ({ sourceId, orderId }) => ({
-        url: 'payment',
-        method: 'POST',
-        body: {
-          sourceId,
-          orderId,
-          idempotencyKey: uuidv4()
-        }
+    createPayment: builder.mutation<void, {
+      sourceId: string,
+      orderId: string }>({
+        query: ({ sourceId, orderId }) => ({
+          url: 'payment',
+          method: 'POST',
+          body: {
+            sourceId,
+            orderId,
+            idempotencyKey: uuidv4()
+          }
+        })
       })
-    })
   }),
   overrideExisting: false
 })
