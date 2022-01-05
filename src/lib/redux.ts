427a5c483395bd9bd33e0646d201bc86d773019f
griffinodow <griffin@griffindow.com>
2022-01-05 14:50:44 -0800

    Add comment to redux store and refactor

--- src/lib/redux.ts
+++ src/lib/redux.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from '../slices/api'
 import { appSlice } from '../slices/app'
 
+/**
+ * The redux store for state management.
+ */
 export const store = configureStore({
   reducer: {
     app: appSlice.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
