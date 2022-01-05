ef5c47eb998c85a9b4053e8c571cfa1188db77c5
griffinodow <griffin@griffindow.com>
2022-01-05 14:52:53 -0800

    Add comment and refactor to app slice

--- src/slices/app.ts
+++ src/slices/app.ts
@@ -1,35 +1,49 @@
 import { createSlice, current, PayloadAction } from '@reduxjs/toolkit'
 
+/**
+ * Line item interface.
+ */
 export interface ILineItem {
   itemId: string
   quantity: number
   catalogObjectId: string
 }
 
+/**
+ * App state interface.
+ */
 export interface IApp {
   lineItems: ILineItem[]
 }
 
+/**
+ * Slice for local app state management.
+ */
 export const appSlice = createSlice({
   name: 'app',
   initialState: { lineItems: <ILineItem[]>[] } as IApp,
   reducers: {
     addLineItem: (state: IApp, action: PayloadAction<ILineItem>): void => {
-      const index = current(state).lineItems.findIndex(item => item.itemId === action.payload.itemId)
+      const index = current(state).lineItems.findIndex(item =>
+        item.itemId === action.payload.itemId)
       if (index !== -1) {
         state.lineItems[index].quantity += action.payload.quantity
       } else {
         state.lineItems.push(action.payload)
       }
     },
-    updateLineItem: (state: IApp, action: PayloadAction<{ id: string, quantity: number}>): void => {
-      const index = current(state).lineItems.findIndex(item => item.itemId === action.payload.id)
+    updateLineItem: (
+      state: IApp,
+      action: PayloadAction<{ id: string, quantity: number}>): void => {
+      const index = current(state).lineItems.findIndex(item =>
+        item.itemId === action.payload.id)
       if (index !== -1) {
         state.lineItems[index].quantity = action.payload.quantity
       }
     },
     removeLineItem: (state: IApp, action: PayloadAction<string>): void => {
-      const index = current(state).lineItems.findIndex(item => item.itemId === action.payload)
+      const index = current(state).lineItems.findIndex(item =>
+        item.itemId === action.payload)
       if (index !== -1) {
         state.lineItems.splice(index, 1)
       }
