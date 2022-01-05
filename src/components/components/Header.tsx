278da0b503f9b7644976c6117321b307bbb8b52b
griffinodow <griffin@griffindow.com>
2022-01-05 15:08:24 -0800

    Add comment and refactor header

--- src/components/components/Header.tsx
+++ src/components/components/Header.tsx
@@ -2,11 +2,22 @@ import { Link } from 'react-router-dom'
 import { CartCounter } from './CartCounter'
 import styles from './Header.module.sass'
 
-export const Header = ({ title, subtitle }: { title: string, subtitle?: string }) => {
+/**
+ * The header component at the top of a page.
+ * @param title The title of the page.
+ * @param subtitle The subtitle of the page.
+ * @returns The Header component.
+ */
+export const Header = ({
+  title,
+  subtitle
+}: { title: string, subtitle?: string }) => {
   return (
     <header className={styles.header}>
       <section>
-        <Link to="/" className={styles.link}><span className='heading'>Home</span></Link>
+        <Link to="/" className={styles.link}>
+          <span className='heading'>Home</span>
+        </Link>
         <CartCounter/>
       </section>
       <section>
