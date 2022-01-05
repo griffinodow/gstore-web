0886f668c9bafa488f3991a935f98eaaad680ad2
griffinodow <griffin@griffindow.com>
2022-01-05 15:08:33 -0800

    Add comment and refactor counter

--- src/components/components/Counter.tsx
+++ src/components/components/Counter.tsx
@@ -1,5 +1,15 @@
 import styles from './Counter.module.sass'
-export const Counter = ({ count, setCount }: { count: number, setCount: Function }) => {
+
+/**
+ * A component that changes with increment and decrement buttons.
+ * @param count the displayed value.
+ * @param setCount Function that changes the displayed value.
+ * @returns The Counter component.
+ */
+export const Counter = ({
+  count,
+  setCount
+}: { count: number, setCount: Function }) => {
   const handleDecrementCount = () => {
     if (count > 1) setCount(count -= 1)
   }
@@ -8,9 +18,13 @@ export const Counter = ({ count, setCount }: { count: number, setCount: Function
   }
   return (
     <div className={styles.counter}>
-      { count > 1 ? <button onClick={() => handleDecrementCount()}>-</button> : <button disabled>-</button> }
+      { count > 1
+        ? <button onClick={() => handleDecrementCount()}>-</button>
+        : <button disabled>-</button> }
       <div>{count.toString()}</div>
-      { count < 99 ? <button onClick={() => handleIncrementCount()}>+</button> : <button disabled>+</button>}
+      { count < 99
+        ? <button onClick={() => handleIncrementCount()}>+</button>
+        : <button disabled>+</button>}
     </div>
   )
 }
