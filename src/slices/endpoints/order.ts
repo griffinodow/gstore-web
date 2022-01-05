2ac5ba8d67c1c5c67d0c993f914a620bc2daf14b
griffinodow <griffin@griffindow.com>
2022-01-05 14:55:46 -0800

    Add comment to order endpoint and reffactor

--- src/slices/endpoints/order.ts
+++ src/slices/endpoints/order.ts
@@ -1,6 +1,9 @@
 import { v4 as uuidv4 } from 'uuid'
 import { apiSlice } from '../api'
 
+/**
+ * Order interface.
+ */
 export interface IOrder {
   success: string,
   data: {
@@ -11,25 +14,33 @@ export interface IOrder {
   }
 }
 
+/**
+ * Submission line item interface.
+ */
 export interface ILineItemSubmit {
   itemId: string
   quantity: string
   catalogObjectId: string
 }
 
+/**
+ * Order endpoint in the api slice.
+ */
 const orderApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    createOrder: builder.mutation<IOrder, { locationId: string, lineItems: ILineItemSubmit[] }>({
-      query: ({ locationId, lineItems }) => ({
-        url: 'order',
-        method: 'POST',
-        body: {
-          locationId,
-          lineItems,
-          idempotencyKey: uuidv4()
-        }
+    createOrder: builder.mutation<IOrder, {
+      locationId: string,
+      lineItems: ILineItemSubmit[] }>({
+        query: ({ locationId, lineItems }) => ({
+          url: 'order',
+          method: 'POST',
+          body: {
+            locationId,
+            lineItems,
+            idempotencyKey: uuidv4()
+          }
+        })
       })
-    })
   }),
   overrideExisting: false
 })
