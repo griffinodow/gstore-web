e0169e06a11cb89da09349e22d36c51ba095d6a3
griffinodow <griffin@griffindow.com>
2022-01-05 15:23:54 -0800

    Add comment to select button

--- src/components/pages/home/components/SelectButton.tsx
+++ src/components/pages/home/components/SelectButton.tsx
@@ -1,5 +1,13 @@
 import styles from './SelectButton.module.sass'
 
+/**
+ * Button used to show selected choice.
+ * @param id The button ID.
+ * @param text The button text.
+ * @param selected If the button is selected.
+ * @param setCategory To change the selected category to this button.
+ * @returns The SelectButton component.
+ */
 export const SelectButton = ({
   id,
   text,
