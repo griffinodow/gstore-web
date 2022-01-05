9758b62d4bc7e406d8e521fd9e25bdfe4d9cbcf4
griffinodow <griffin@griffindow.com>
2022-01-05 15:01:00 -0800

    Add comment and refactor button component

--- src/components/components/Button.tsx
+++ src/components/components/Button.tsx
@@ -1,9 +1,24 @@
 import styles from './Button.module.sass'
 
-export const Button = ({ text, onClick }: { text: string, onClick?: Function }) => {
+/**
+ * Generic button component.
+ * @param param Display text, optional click function.
+ * @returns Button component.
+ */
+export const Button = ({
+  text,
+  onClick
+}: {
+  text: string,
+  onClick?: Function
+}) => {
   return (
     onClick
-      ? <button onClick={() => onClick()} className={styles.button}>{text}</button>
+      ? <button
+          onClick={() => onClick()}
+          className={styles.button}>
+            {text}
+        </button>
       : <button className={styles.button}>{text}</button>
   )
 }
