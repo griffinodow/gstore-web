e87d9e12c35aee9dbb2c28c1fde14eb47a0134c1
griffinodow <griffin@griffindow.com>
2022-01-05 15:24:05 -0800

    Add comment to product component

--- src/components/pages/home/components/Product.tsx
+++ src/components/pages/home/components/Product.tsx
@@ -1,7 +1,27 @@
 import { Link } from 'react-router-dom'
 import styles from './Product.module.sass'
 
-export const Product = ({ id, name, price, description, url }: { id: string, name: string, price: number, description: string, url: string }) => {
+/**
+ * Shows a product listing on the home page.
+ * @param id The product ID.
+ * @param name The product name.
+ * @param price The product price.
+ * @param description The product description.
+ * @param url The product image url.
+ * @returns The Product component.
+ */
+export const Product = ({
+  id,
+  name,
+  price,
+  description,
+  url
+}: {
+  id: string,
+  name: string,
+  price: number,
+  description: string,
+  url: string }) => {
   return (
     <Link to={`/products/${id}`} className={styles.product}>
       <section>
