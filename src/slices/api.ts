d7204d17cde43364d308d0780cfe23a3726c0ad2
griffinodow <griffin@griffindow.com>
2022-02-02 11:11:58 -0800

    Update api url

--- src/slices/api.ts
+++ src/slices/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
  * Slice for api queries.
  */
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.g-store.griffindow.com/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api-g-store.griffindow.com/' }),
   tagTypes: ['Catalog'],
   endpoints: () => ({})
 })
